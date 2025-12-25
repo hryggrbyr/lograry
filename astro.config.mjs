339d39ed5b1f2410c2a0b9eaac3ed674760c2264
Thomas Rigby <thom@scrummable.com>
2025-12-25 21:36:01 +0000

    fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,7 @@ import netlify from "@astrojs/netlify";
 export default defineConfig({
   output: "server", // or 'hybrid'
   adapter: netlify(),
+  build: {
+    assets: "dist", // This tells Astro where to put the bundled assets
+  },
 });
