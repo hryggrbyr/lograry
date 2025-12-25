a136981e48f4821e4812c917cb3649d3d0b32739
hryggrbyr <113116722+hryggrbyr@users.noreply.github.com>
2025-12-25 21:47:04 +0000

    Simplify astro.config.mjs by removing build config
    Removed the build configuration from astro.config.mjs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,5 @@ import netlify from "@astrojs/netlify";
 
 export default defineConfig({
   output: "server", // or 'hybrid'
-  adapter: netlify(),
-  build: {
-    assets: "public", // This tells Astro where to put the bundled assets
-  },
+  adapter: netlify()
 });
