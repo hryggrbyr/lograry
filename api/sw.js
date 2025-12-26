2e500d5266a09f1c9da5bf975693220b24b6db7a
hryggrbyr <hryggrbyr@users.noreply.github.com>
2025-12-26 22:31:44 +0000

    deploy: 500602f99058e3e2649b1015b15ec1e4a038ef12

---
+++ api/sw.js
@@ -0,0 +1,26 @@
+
+    const CACHE_NAME = 'lograry-admin-cache-v1';
+    const urlsToCache = ["/admin/","/admin/config.yml","/admin/preview.css","https://unpkg.com/@sveltia/cms@latest/dist/sveltia-cms.js"];
+
+    self.addEventListener('install', event => {
+      event.waitUntil(
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+          })
+      );
+    });
+
+    self.addEventListener('fetch', event => {
+      event.respondWith(
+        caches.match(event.request)
+          .then(response => {
+            if (response) {
+              return response;
+            }
+            return fetch(event.request);
+          })
+      );
+    });
+  
