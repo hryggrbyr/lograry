1ce0f1dbadecbcb0c3c16147ade4e8af709675c9
Thomas Rigby <thom@scrummable.com>
2025-12-26 22:37:30 +0000

    adds prod detection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 
+const isProd = process.env.NODE_ENV === "production";
+
 // https://astro.build/config
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   output: "static",
   site: "https://hryggrbyr.github.io",
-  base: command === "build" ? "/lograry/" : "/",
-}));
+  base: isProd ? "/lograry/" : "/",
+});
